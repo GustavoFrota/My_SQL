c21b917a230fefe298dcff62b229c6c55d9cb27c
GustavoFrota <gustavofro90@gmail.com>
2026-02-24 08:30:28 -0300

    Adicionando dados

--- cadastro.sql
+++ cadastro.sql
@@ -8,3 +8,11 @@ create table cadastro(
     nacionalidade varchar(20) default 'Brasil',
     primary key(id)
 );
+insert into cadastro(nome, nascimento, sexo, peso, altura, nacionalidade) values
+('Gabriel', '2007-01-02', 'M', 78.5, 1.83, default),
+('Maria', '1999-12-30', 'F', 55.2, 1.65, 'Portugal'),
+('Creusa', '1920-11-25', 'F', 50.2, 1.65, default),
+('Adalgiza', '1930-11-2', 'F', 63.2, 1.75, 'Irlanda'),
+('Ana', '1975-12-22', 'F', 52.3, 1.43, 'EUA'),
+('Pedro', '2000-05-30', 'M', 75.9, 1.70, default);
+select * from cadastro;
