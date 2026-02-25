58960b310a3677eac9157cef08ce7219c4bbcbb5
GustavoFrota <gustavofro90@gmail.com>
2026-02-25 09:03:09 -0300

    Adicionando dados

--- cursos.sql
+++ cursos.sql
@@ -11,3 +11,17 @@ add id_curso int first;
 
 alter table
 add primary key(id_curso);
+
+insert into cursos(id_curso, nome, descricao, carga, total_aulas, ano) values
+(1, 'HTML4', 'Curso de HTML5', 40, 37, '2014'),
+(2, 'Algoritmos', 'Lógica de Programação', 20, 15, '2014'),
+(3, 'Photoshop', 'Dicas de Photoshop CC', 10, 8, '2014'),
+(4, 'PGP', 'Curso de PHP para iniciantes', 40, 20, '2010'),
+(5, 'Jarva', 'Introdução à Linguagem Java', 10, 29, '2000'),
+(6, 'MySQL', 'Bancos de Dados MySQL', 30, 15, default),
+(7, 'World', 'Curso completo de world', 40, 30, default),
+(8, 'Sapateado', 'Danças Rítimicas', 40, 30, '2018'),
+(9, 'Cozinha Árabe', 'Aprender a fazer Kibe', 40, 30, '2018'),
+(10, 'YouTuber', 'Gerar polêmica e ganhar inscritos', 5, 2, '2018');
+
+select * from cursos;
